7e69a565dbac9a51a275a6a1ca9aaad52a8d1c3b
jy <977996067@qq.com>
2020-03-04 15:33:28 +0800

    :sparkles: maximum-subarray

---
+++ src/test/java/io/github/jjyyjjyy/problem/MaximumSubarrayTest.java
@@ -0,0 +1,22 @@
+package io.github.jjyyjjyy.problem;
+
+import io.github.jjyyjjyy.util.CsvToArray;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author jy
+ */
+class MaximumSubarrayTest {
+
+    private MaximumSubarray maximumSubarray = new MaximumSubarray();
+
+    @ParameterizedTest
+    @CsvSource({"'-2,1,-3,4,-1,2,1,-5,4',6"})
+    void maxSubArray(@ConvertWith(CsvToArray.class) int[] param, int result) {
+        assertEquals(result, maximumSubarray.maxSubArray(param));
+    }
+}
