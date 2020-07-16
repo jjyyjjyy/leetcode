1ae43412852990d7abbeb24bd949389d9517becc
jy <977996067@qq.com>
2020-07-16 09:20:40 +0800

    :sparkles: Add move-zeroes solution

--- src/main/java/io/github/jjyyjjyy/problem/MoveZeroes.java
+++ src/main/java/io/github/jjyyjjyy/problem/MoveZeroes.java
@@ -1,5 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
@@ -35,6 +36,42 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class MoveZeroes {
 
+    /**
+     * 1. 维护快慢两个指针, 慢指针指向遍历出来的最后一个非0元素的位置.
+     * 2. 如果快指针所在的元素不为0, 则交换快慢指针两个位置上的元素, 慢指针前进一位.
+     * 3. 快指针前进一位.
+     */
+    @Complexity(Complexity.ComplexityType.O_N)
     public void moveZeroes(int[] nums) {
+        int last = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                swap(nums, last++, i);
+            }
+        }
+    }
+
+    private void swap(int[] arr, int x, int y) {
+        int tmp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = tmp;
+    }
+
+    /**
+     * 1. 维护快慢两个指针, 其中慢指针的前导元素都为0.
+     * 2. 依次遍历数组, 如果快指针所在元素非0, 则就赋值到慢指针位置, 慢指针前进一位.
+     * 3. 快指针前进一位.
+     */
+    @Complexity(Complexity.ComplexityType.O_N)
+    public void moveZeroes2(int[] nums) {
+        int last = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[last++] = nums[i];
+            }
+        }
+        for (int i = last; i < nums.length; i++) {
+            nums[i] = 0;
+        }
     }
 }
