f15518f44edb1012d26844759a58ffde81c33918
jy <977996067@qq.com>
2020-07-20 10:51:23 +0800

    :sparkles: Add 3sum solution

--- src/main/java/io/github/jjyyjjyy/problem/ThreeSum.java
+++ src/main/java/io/github/jjyyjjyy/problem/ThreeSum.java
@@ -1,9 +1,12 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -44,7 +47,46 @@ import java.util.List;
 )
 public class ThreeSum {
 
+    /**
+     * 现将数组排序, 维护左指针i, 然后从右侧数组中找到和为-nums[i]的两个元素(同2sum).
+     */
+    @Complexity(Complexity.ComplexityType.O_N_POW_2)
     public List<List<Integer>> threeSum(int[] nums) {
-        return Collections.emptyList();
+        if (nums.length < 3) {
+            return Collections.emptyList();
+        }
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int target = -nums[i];
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int leftValue = nums[left];
+                int rightValue = nums[right];
+                if (leftValue + rightValue == target) {
+                    result.add(Arrays.asList(nums[i], leftValue, rightValue));
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } else if (leftValue + rightValue < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
     }
 }
