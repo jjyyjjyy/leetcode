9d8bdea879e5cde41202b01c2540c42805a45480
jy <977996067@qq.com>
2020-07-27 10:13:48 +0800

    :sparkles: Add find-first-and-last-position-of-element-in-sorted-array solution

--- src/main/java/io/github/jjyyjjyy/problem/FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/io/github/jjyyjjyy/problem/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -1,5 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
@@ -37,7 +38,68 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class FindFirstAndLastPositionOfElementInSortedArray {
 
+    /**
+     * 1. 使用二分法找到目标元素.
+     * 2. 向左遍历一直找到和目标元素相等的最左的元素位置.
+     * 3. 向右遍历一直找到和目标元素相等的最右的元素位置.
+     */
+    @Complexity(Complexity.ComplexityType.O_LOG_N)
     public int[] searchRange(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                int l = mid;
+                int r = mid;
+                while (l > 0 && nums[l - 1] == target) {
+                    l--;
+                }
+                while (r < nums.length - 1 && nums[r + 1] == target) {
+                    r++;
+                }
+                return new int[]{l, r};
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
         return new int[]{-1, -1};
+    }
+
+    /**
+     * 从左向右, 从右向左遍历, 找到目标元素.
+     */
+    @Complexity(Complexity.ComplexityType.O_N)
+    public int[] searchRange2(int[] nums, int target) {
+        int[] result = {-1, -1};
+
+        int left = 0, right = nums.length - 1;
+        boolean leftFounded = false, rightFounded = false;
+        while (left <= right) {
+            if (!leftFounded) {
+                if (nums[left] == target) {
+                    result[0] = left;
+                    leftFounded = true;
+                } else {
+                    left++;
+                }
+            }
+            if (!rightFounded) {
+                if (nums[right] == target) {
+                    result[1] = right;
+                    rightFounded = true;
+                } else {
+                    right--;
+                }
+            }
+            if (leftFounded && rightFounded) {
+                break;
+            }
+        }
+        return result;
     }
 }
