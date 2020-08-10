08871f9a53f4fe6e6f4eeda714948f796e702817
jy <977996067@qq.com>
2020-08-10 10:48:50 +0800

    :sparkles: Add minimum-path-sum solution

--- src/main/java/io/github/jjyyjjyy/problem/MinimumPathSum.java
+++ src/main/java/io/github/jjyyjjyy/problem/MinimumPathSum.java
@@ -1,5 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
@@ -39,7 +40,33 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class MinimumPathSum {
 
+    /**
+     * dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
+     */
+    @Complexity(Complexity.ComplexityType.O_M_N)
     public int minPathSum(int[][] grid) {
-        return 0;
+
+        int m = grid.length;
+        if (m == 0) {
+            return 0;
+        }
+        int n = grid[0].length;
+
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = grid[i][0] + dp[i - 1][0];
+        }
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = grid[0][i] + dp[0][i - 1];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 }
