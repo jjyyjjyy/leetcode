df60dcd159dfcc5f31be6e89dd72eb6784db504d
jy <977996067@qq.com>
2020-08-17 18:01:57 +0800

    :sparkles: Add set-matrix-zeroes solution

--- src/main/java/io/github/jjyyjjyy/problem/SetMatrixZeroes.java
+++ src/main/java/io/github/jjyyjjyy/problem/SetMatrixZeroes.java
@@ -1,5 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
@@ -62,6 +63,48 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class SetMatrixZeroes {
 
+    @Complexity(Complexity.ComplexityType.O_M_N)
     public void setZeroes(int[][] matrix) {
+
+        int m = matrix.length;
+        if (m == 0) {
+            return;
+        }
+        int n = matrix[0].length;
+        boolean firstColumnZeroed = false;
+
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColumnZeroed = true;
+            }
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // 第0行
+        if (matrix[0][0] == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+
+        // 第0列
+        if (firstColumnZeroed) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
