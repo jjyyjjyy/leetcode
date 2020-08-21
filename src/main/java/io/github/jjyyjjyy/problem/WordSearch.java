19cb5a8b468f0d6e693a984381c2bebf052e3d48
jy <977996067@qq.com>
2020-08-21 11:27:49 +0800

    :sparkles: Add word-search solution

--- src/main/java/io/github/jjyyjjyy/problem/WordSearch.java
+++ src/main/java/io/github/jjyyjjyy/problem/WordSearch.java
@@ -1,5 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
@@ -38,7 +39,63 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class WordSearch {
 
+    private char[][] board;
+
+    private char[] charArray;
+
+    private boolean[][] marked;
+
+    private int m;
+
+    private int n;
+
+    /**
+     * 依次遍历矩阵, 将每个元素和其周围元素(上右下左方向)与给定字符做比较.
+     */
+    @Complexity(Complexity.ComplexityType.O_M_N)
     public boolean exist(char[][] board, String word) {
+        this.m = board.length;
+        if (this.m == 0) {
+            return false;
+        }
+        this.n = board[0].length;
+        this.marked = new boolean[m][n];
+        this.board = board;
+        this.charArray = word.toCharArray();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(int i, int j, int index) {
+        int c = board[i][j];
+        char target = charArray[index];
+        if (index == charArray.length - 1 && c == target) {
+            return true;
+        }
+        if (c == target) {
+            marked[i][j] = true;
+            index++;
+            if (i > 0 && !marked[i - 1][j] && dfs(i - 1, j, index)) {
+                return true;
+            }
+            if (j < n - 1 && !marked[i][j + 1] && dfs(i, j + 1, index)) {
+                return true;
+            }
+            if (i < m - 1 && !marked[i + 1][j] && dfs(i + 1, j, index)) {
+                return true;
+            }
+            if (j > 0 && !marked[i][j - 1] && dfs(i, j - 1, index)) {
+                return true;
+            }
+            marked[i][j] = false;
+        }
         return false;
     }
 
