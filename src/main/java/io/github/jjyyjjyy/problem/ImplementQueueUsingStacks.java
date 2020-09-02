930693828a3a0004783c010341ac959ea16cc7a6
jy <977996067@qq.com>
2020-09-02 09:50:32 +0800

    :sparkles: implement-queue-using-stacks

--- src/main/java/io/github/jjyyjjyy/problem/ImplementQueueUsingStacks.java
+++ src/main/java/io/github/jjyyjjyy/problem/ImplementQueueUsingStacks.java
@@ -46,5 +46,33 @@ import io.github.jjyyjjyy.core.Tag;
     difficulty = Difficulty.EASY,
     tags = {Tag.STACK, Tag.DESIGN}
 )
-public class ImplementQueueusingStacks {
+public class ImplementQueueUsingStacks {
+
+    /**
+     * Push element x to the back of queue.
+     */
+    public void push(int x) {
+
+    }
+
+    /**
+     * Removes the element from in front of queue and returns that element.
+     */
+    public int pop() {
+        return -1;
+    }
+
+    /**
+     * Get the front element.
+     */
+    public int peek() {
+        return -1;
+    }
+
+    /**
+     * Returns whether the queue is empty.
+     */
+    public boolean empty() {
+        return false;
+    }
 }
