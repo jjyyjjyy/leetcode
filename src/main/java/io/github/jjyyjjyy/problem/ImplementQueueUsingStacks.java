0c8c3500c217afd48990ace8bc70f0585e0aa317
jy <977996067@qq.com>
2020-09-02 09:51:19 +0800

    :sparkles: Add implement-queue-using-stacks solution

--- src/main/java/io/github/jjyyjjyy/problem/ImplementQueueUsingStacks.java
+++ src/main/java/io/github/jjyyjjyy/problem/ImplementQueueUsingStacks.java
@@ -4,6 +4,8 @@ import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
 
+import java.util.Stack;
+
 /**
  * <a href="https://leetcode-cn.com/problems/implement-queue-using-stacks/">用栈实现队列</a>
  *
@@ -48,31 +50,58 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class ImplementQueueUsingStacks {
 
+    private final Stack<Integer> stack = new Stack<>();
+
     /**
      * Push element x to the back of queue.
      */
     public void push(int x) {
-
+        stack.push(x);
     }
 
     /**
      * Removes the element from in front of queue and returns that element.
      */
     public int pop() {
-        return -1;
+
+        if (stack.isEmpty()) {
+            return -1;
+        }
+
+        Stack<Integer> helpStack = new Stack<>();
+        while (!stack.isEmpty()) {
+            helpStack.push(stack.pop());
+        }
+        int result = helpStack.pop();
+        while (!helpStack.isEmpty()) {
+            stack.push(helpStack.pop());
+        }
+        return result;
     }
 
     /**
      * Get the front element.
      */
     public int peek() {
-        return -1;
+        if (stack.isEmpty()) {
+            return -1;
+        }
+
+        Stack<Integer> helpStack = new Stack<>();
+        while (!stack.isEmpty()) {
+            helpStack.push(stack.pop());
+        }
+        int result = helpStack.peek();
+        while (!helpStack.isEmpty()) {
+            stack.push(helpStack.pop());
+        }
+        return result;
     }
 
     /**
      * Returns whether the queue is empty.
      */
     public boolean empty() {
-        return false;
+        return stack.isEmpty();
     }
 }
