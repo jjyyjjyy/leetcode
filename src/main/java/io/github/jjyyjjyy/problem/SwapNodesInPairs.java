929c5c1bc36cc0c7e75f8f1cfd93e0342c73ce6e
jy <977996067@qq.com>
2020-09-16 09:53:51 +0800

    :sparkles: Add swap-nodes-in-pairs solution

--- src/main/java/io/github/jjyyjjyy/problem/SwapNodesInPairs.java
+++ src/main/java/io/github/jjyyjjyy/problem/SwapNodesInPairs.java
@@ -1,9 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
-import io.github.jjyyjjyy.core.Difficulty;
-import io.github.jjyyjjyy.core.ListNode;
-import io.github.jjyyjjyy.core.Problem;
-import io.github.jjyyjjyy.core.Tag;
+import io.github.jjyyjjyy.core.*;
 
 /**
  * <a href="https://leetcode-cn.com/problems/swap-nodes-in-pairs/">两两交换链表中的节点</a>
@@ -33,7 +30,29 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class SwapNodesInPairs {
 
+    /**
+     * 1. 构造一个null节点, 指向head.
+     * 2. 声明一个prev指针指向之前的节点(默认为dummy), 一个current指针指向当前节点(默认为head).
+     * 3. 将current和current.next交换, 然后prev指向current, current指向current.next.next.
+     * 4. 一直遍历到current&current.next为空.
+     */
+    @Complexity(Complexity.ComplexityType.O_N)
     public ListNode swapPairs(ListNode head) {
-        return null;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            ListNode next = current.next;
+            ListNode nextCurrent = next.next;
+            prev.next = next;
+            current.next = nextCurrent;
+            next.next = current;
+            prev = current;
+            current = nextCurrent;
+        }
+
+        return dummy.next;
     }
 }
