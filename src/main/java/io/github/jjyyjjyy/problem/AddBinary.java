54e84856d7f234e16358bfa311833f376341d809
jy <977996067@qq.com>
2020-10-10 14:45:57 +0800

    :sparkles: Add add-binary solution

--- src/main/java/io/github/jjyyjjyy/problem/AddBinary.java
+++ src/main/java/io/github/jjyyjjyy/problem/AddBinary.java
@@ -1,5 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
@@ -38,7 +39,41 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class AddBinary {
 
+    /**
+     * 将两个数字从后向前遍历, 依次相加.
+     * 如果加得的结果大于1, 则前一位相加的时候要加上1, 以此类推.
+     */
+    @Complexity(Complexity.ComplexityType.O_M_AND_N)
     public String addBinary(String a, String b) {
-        return null;
+        char[] adderA = a.toCharArray();
+        char[] adderB = b.toCharArray();
+
+        StringBuilder result = new StringBuilder();
+
+        int lengthA = adderA.length;
+        int lengthB = adderB.length;
+        int last = 0;
+        int positionA = lengthA - 1;
+        int positionB = lengthB - 1;
+
+        while (positionA >= 0 || positionB >= 0) {
+            int valueA = positionA < 0 ? 0 : (adderA[positionA] - '0');
+            int valueB = positionB < 0 ? 0 : (adderB[positionB] - '0');
+
+            int add = valueA + valueB + last;
+            if (add > 1) {
+                add -= 2;
+                last = 1;
+            } else {
+                last = 0;
+            }
+            result.append(add);
+            positionA--;
+            positionB--;
+        }
+        if (last == 1) {
+            result.append(1);
+        }
+        return result.reverse().toString();
     }
 }
