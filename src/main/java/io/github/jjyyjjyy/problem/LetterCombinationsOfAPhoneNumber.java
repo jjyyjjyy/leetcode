5e2f1dd1e74cb3de2a971437e43a98d9b1f66878
jy <977996067@qq.com>
2020-10-22 11:03:57 +0800

    :sparkles: Add letter-combinations-of-a-phone-number solution

--- src/main/java/io/github/jjyyjjyy/problem/LetterCombinationsOfAPhoneNumber.java
+++ src/main/java/io/github/jjyyjjyy/problem/LetterCombinationsOfAPhoneNumber.java
@@ -1,10 +1,13 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <a href="https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/">电话号码的字母组合</a>
@@ -40,8 +43,31 @@ import java.util.List;
 )
 public class LetterCombinationsOfAPhoneNumber {
 
+    private static final Map<Character, String> PHONE_MAP = Map.of('2', "abc", '3', "def", '4', "ghi", '5', "jkl", '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
+
+    @Complexity(value = Complexity.ComplexityType.O_DEFINE, complexity = "O(3^m*4^n)")
     public List<String> letterCombinations(String digits) {
-        return null;
+
+        if (digits.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        List<String> result = new ArrayList<>();
+        backTracking(result, digits, 0, new StringBuilder());
+        return result;
     }
 
+    private void backTracking(List<String> result, String digits, int i, StringBuilder combination) {
+        if (i == digits.length()) {
+            result.add(combination.toString());
+            return;
+        }
+        char c = digits.charAt(i);
+        String phone = PHONE_MAP.get(c);
+        for (int j = 0; j < phone.length(); j++) {
+            combination.append(phone.charAt(j));
+            backTracking(result, digits, i + 1, combination);
+            combination.deleteCharAt(i);
+        }
+    }
 }
