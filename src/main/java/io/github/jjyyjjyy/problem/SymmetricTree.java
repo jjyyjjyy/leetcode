973f7a77440d22693a93ad0d300ea6efc89c3dc4
jy <977996067@qq.com>
2020-11-14 14:57:41 +0800

    :sparkles: Add symmetric-tree solution

--- src/main/java/io/github/jjyyjjyy/problem/SymmetricTree.java
+++ src/main/java/io/github/jjyyjjyy/problem/SymmetricTree.java
@@ -1,9 +1,10 @@
 package io.github.jjyyjjyy.problem;
 
-import io.github.jjyyjjyy.core.Difficulty;
-import io.github.jjyyjjyy.core.Problem;
-import io.github.jjyyjjyy.core.Tag;
-import io.github.jjyyjjyy.core.TreeNode;
+import io.github.jjyyjjyy.core.*;
+
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Queue;
 
 /**
  * <a href="https://leetcode-cn.com/problems/symmetric-tree/">对称二叉树</a>
@@ -40,7 +41,57 @@ import io.github.jjyyjjyy.core.TreeNode;
 )
 public class SymmetricTree {
 
+    /**
+     * 递归遍历左右子树, 判断是否 left.left == right.right && left.right == right.left.
+     */
+    @Complexity(Complexity.ComplexityType.O_N)
     public boolean isSymmetric(TreeNode root) {
-        return false;
+        if (root == null) {
+            return false;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    /**
+     * 1. 维护一个队列, 首先加入root的左右子树.
+     * 2. 将队列最后两个子树取出, 判断两个子树是否相等.
+     * 2.1. 如果不相等, 则返回false.
+     * 2.2. 如果相等再分别将left.left&right.right和left.right&right.left放入队列中.
+     * 3. 重复步骤2, 直到队列为空.
+     */
+    @Complexity(Complexity.ComplexityType.O_N)
+    public boolean isSymmetric2(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+
+        while (!queue.isEmpty()) {
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+            if (left == null && right == null) {
+                continue;
+            }
+            if (left == null || right == null || !Objects.equals(left.val, right.val)) {
+                return false;
+            }
+            queue.add(left.left);
+            queue.add(right.right);
+            queue.add(left.right);
+            queue.add(right.left);
+        }
+        return true;
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null || !Objects.equals(left.val, right.val)) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
     }
 }
