9c946e92d7f8c31a895ebc937c16742816a9b015
jy <977996067@qq.com>
2020-11-14 14:56:54 +0800

    :sparkles: symmetric-tree

--- src/main/java/io/github/jjyyjjyy/problem/SymmetricTree.java
+++ src/main/java/io/github/jjyyjjyy/problem/SymmetricTree.java
@@ -3,6 +3,7 @@ package io.github.jjyyjjyy.problem;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
+import io.github.jjyyjjyy.core.TreeNode;
 
 /**
  * <a href="https://leetcode-cn.com/problems/symmetric-tree/">对称二叉树</a>
@@ -38,4 +39,8 @@ import io.github.jjyyjjyy.core.Tag;
     tags = {Tag.TREE, Tag.DEPTH_FIRST_SEARCH, Tag.BREADTH_FIRST_SEARCH}
 )
 public class SymmetricTree {
+
+    public boolean isSymmetric(TreeNode root) {
+        return false;
+    }
 }
--- src/test/java/io/github/jjyyjjyy/util/CsvToTreeNode.java
+++ src/test/java/io/github/jjyyjjyy/util/CsvToTreeNode.java
@@ -16,24 +16,21 @@ public class CsvToTreeNode extends SimpleArgumentConverter {
     @Override
     protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
         String sourceString = source.toString();
-        if ("null".equals(sourceString) || sourceString.isEmpty()) {
+        if (sourceString.isEmpty() || "null".equals(sourceString)) {
             return null;
         }
         String[] strings = sourceString.split(ARRAY_SPLITTER_REGEX);
-        Integer[] arr = Arrays.stream(strings).map(s -> s.isEmpty() ? null : Integer.parseInt(s)).toArray(Integer[]::new);
+        Integer[] arr = Arrays.stream(strings).map(s -> s.isEmpty() || "null".equals(s) ? null : Integer.parseInt(s)).toArray(Integer[]::new);
 
         if (arr.length == 0) {
             return null;
         }
-        TreeNode root = new TreeNode(arr[0]);
-        root.left = buildTreeNode(new TreeNode(), arr, 1);
-        root.right = buildTreeNode(new TreeNode(), arr, 2);
-        return root;
+        return buildTreeNode(new TreeNode(), arr, 0);
     }
 
     private TreeNode buildTreeNode(TreeNode root, Integer[] arr, int idx) {
         if (idx >= arr.length) {
-            return root;
+            return null;
         }
         root.val = arr[idx];
         root.left = buildTreeNode(new TreeNode(), arr, idx * 2 + 1);
