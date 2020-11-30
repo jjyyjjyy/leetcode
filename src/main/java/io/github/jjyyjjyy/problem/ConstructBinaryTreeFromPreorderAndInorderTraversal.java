28a546625d4c5c6d976f9087ef3f9950f576ce47
jy <977996067@qq.com>
2020-11-30 14:22:10 +0800

    :sparkles: Add construct-binary-tree-from-preorder-and-inorder-traversal solution

--- src/main/java/io/github/jjyyjjyy/problem/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/main/java/io/github/jjyyjjyy/problem/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -1,9 +1,9 @@
 package io.github.jjyyjjyy.problem;
 
-import io.github.jjyyjjyy.core.Difficulty;
-import io.github.jjyyjjyy.core.Problem;
-import io.github.jjyyjjyy.core.Tag;
-import io.github.jjyyjjyy.core.TreeNode;
+import io.github.jjyyjjyy.core.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <a href="https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/">从前序与中序遍历序列构造二叉树</a>
@@ -40,7 +40,35 @@ import io.github.jjyyjjyy.core.TreeNode;
 )
 public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
 
+    private static final Map<Integer, Integer> indexes = new HashMap<>();
+
+    /**
+     * 前序遍历: 根左右
+     * 中序遍历: 左根右
+     * 1. 将中序遍历元素与索引关联关系暂存起来.
+     * 2. 当前前序遍历第一个节点为根节点, 从中序遍历中找到根节点, 可以计算出左右子树的长度, 然后递归遍历左右子树.
+     */
+    @Complexity(Complexity.ComplexityType.O_N)
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return null;
+        int n = inorder.length;
+        indexes.clear();
+        for (int i = 0; i < n; i++) {
+            indexes.put(inorder[i], i);
+        }
+        return buildTree(preorder, 0, n - 1, 0, n - 1);
+    }
+
+    private TreeNode buildTree(int[] preorder, int preorderLeft, int preorderRight, int inorderLeft, int inorderRight) {
+        if (preorderLeft > preorderRight) {
+            return null;
+        }
+        int rootVal = preorder[preorderLeft];
+        TreeNode root = new TreeNode(rootVal);
+
+        int inorderRootIndex = indexes.get(rootVal);
+        int leftCount = inorderRootIndex - inorderLeft;
+        root.left = buildTree(preorder, preorderLeft + 1, preorderLeft + leftCount, inorderLeft, inorderRootIndex - 1);
+        root.right = buildTree(preorder, preorderLeft + leftCount + 1, preorderRight, inorderRootIndex + 1, inorderRight);
+        return root;
     }
 }
