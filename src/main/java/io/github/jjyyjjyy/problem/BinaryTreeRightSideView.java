9fdbe189ac5853f0d2bc5b54c15aff58b7227a29
jy <977996067@qq.com>
2020-12-08 10:28:09 +0800

    :sparkles: binary-tree-right-side-view

--- src/main/java/io/github/jjyyjjyy/problem/BinaryTreeRightSideView.java
+++ src/main/java/io/github/jjyyjjyy/problem/BinaryTreeRightSideView.java
@@ -3,6 +3,9 @@ package io.github.jjyyjjyy.problem;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
+import io.github.jjyyjjyy.core.TreeNode;
+
+import java.util.List;
 
 /**
  * <a href="https://leetcode-cn.com/problems/binary-tree-right-side-view/">二叉树的右视图</a>
@@ -36,4 +39,8 @@ import io.github.jjyyjjyy.core.Tag;
     tags = {Tag.TREE, Tag.DEPTH_FIRST_SEARCH, Tag.BREADTH_FIRST_SEARCH}
 )
 public class BinaryTreeRightSideView {
+
+    public List<Integer> rightSideView(TreeNode root) {
+        return null;
+    }
 }
