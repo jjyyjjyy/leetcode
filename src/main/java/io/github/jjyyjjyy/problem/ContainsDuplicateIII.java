949650f55b7b3bba9c7461cec0343c0beea3e722
jy <977996067@qq.com>
2020-12-15 11:54:03 +0800

    :sparkles: Add contains-duplicate-iii solution

--- src/main/java/io/github/jjyyjjyy/problem/ContainsDuplicateIII.java
+++ src/main/java/io/github/jjyyjjyy/problem/ContainsDuplicateIII.java
@@ -1,8 +1,13 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
 
 /**
  * <a href="https://leetcode-cn.com/problems/contains-duplicate-iii/">存在重复元素 III</a>
@@ -39,8 +44,67 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class ContainsDuplicateIII {
 
+    /**
+     * 1. 维护一个哈希表, 保存桶序号和元素的对应关系.
+     * 2. 依次遍历数组, 计算出当前元素的序号.
+     * 2.1. 如果哈希表中存在该序号, 则说明符合条件的元素, 返回true.
+     * 2.2. 如果哈希表中前一个桶和后一个桶中存在于当前元素相差为t的元素, 则返回true.
+     * 2.3. 将当前元素和序号放到桶中.
+     * 2.4. 将桶中序号为i-k的元素删除掉.
+     */
+    @Complexity(Complexity.ComplexityType.O_N)
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (t < 0) {
+            return false;
+        }
+        Map<Long, Long> bucket = new HashMap<>();
+        long w = (long) t + 1;
+        for (int i = 0; i < nums.length; i++) {
+            long id = getId(nums[i], w);
+            if (bucket.containsKey(id)) {
+                return true;
+            }
+            if (bucket.containsKey(id + 1) && bucket.get(id + 1) - nums[i] < w) {
+                return true;
+            }
+            if (bucket.containsKey(id - 1) && nums[i] - bucket.get(id - 1) < w) {
+                return true;
+            }
+            bucket.put(id, (long) nums[i]);
+            if (i >= k) {
+                bucket.remove(getId(nums[i - k], w));
+            }
+        }
         return false;
     }
 
+    /**
+     * 1. 维护一个有序二叉树.
+     * 2. 依次遍历数组, 判断当前数值在树中是否存在相差为t的前驱节点和后继节点, 如果存在则返回true.
+     * 3. 将当前元素插入到树中, 并将i-k的元素从树中剪枝掉.
+     */
+    @Complexity(Complexity.ComplexityType.O_N_LOG_N)
+    public boolean containsNearbyAlmostDuplicate2(int[] nums, int k, int t) {
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            Integer ceiling = treeSet.ceiling(num);
+            if (ceiling != null && ceiling <= (long) num + t) {
+                return true;
+            }
+            Integer floor = treeSet.floor(num);
+            if (floor != null && num <= (long) floor + t) {
+                return true;
+            }
+            treeSet.add(num);
+            if (treeSet.size() > k) {
+                treeSet.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+
+    private long getId(long num, long w) {
+        return num >= 0 ? num / w : (num + 1) / w - 1;
+    }
 }
