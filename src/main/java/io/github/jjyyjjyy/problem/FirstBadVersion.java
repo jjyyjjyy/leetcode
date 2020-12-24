2c21ce0c063a8ee5c75b8bcbe1746db5c0177022
jy <977996067@qq.com>
2020-12-24 09:47:52 +0800

    :sparkles: Add first-bad-version solution

--- src/main/java/io/github/jjyyjjyy/problem/FirstBadVersion.java
+++ src/main/java/io/github/jjyyjjyy/problem/FirstBadVersion.java
@@ -1,5 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
@@ -39,8 +40,37 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class FirstBadVersion {
 
+    /**
+     * 1. 二分查找, 判断mid是不是badVersion.
+     * 1.1. 如果mid是badVersion, 则将搜索区间改为[left, mid].
+     * 1.2. 如果mid不是badVersion, 则将搜索区间改为[mid+1, right].
+     * 2. 一直遍历到right>=left, 此时left就是最小的badVersion.
+     */
+    @Complexity(Complexity.ComplexityType.O_LOG_N)
     public int firstBadVersion(int n) {
-        return 0;
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    /**
+     * 依次从1遍历到n, 找到最小的badVersion.
+     */
+    @Complexity(Complexity.ComplexityType.O_N)
+    public int firstBadVersion2(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (isBadVersion(i)) {
+                return i;
+            }
+        }
+        return n;
     }
 
     private boolean isBadVersion(int version) {
