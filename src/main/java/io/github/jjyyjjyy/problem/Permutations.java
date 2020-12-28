7665c9c1162bfa41adfface37ddc962aa8eaea22
jy <977996067@qq.com>
2020-12-28 16:38:49 +0800

    :sparkles: Add permutations solution

--- src/main/java/io/github/jjyyjjyy/problem/Permutations.java
+++ src/main/java/io/github/jjyyjjyy/problem/Permutations.java
@@ -1,10 +1,14 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * <a href="https://leetcode-cn.com/problems/permutations/">全排列</a>
@@ -41,8 +45,31 @@ import java.util.List;
 )
 public class Permutations {
 
+    /**
+     * 依次遍历数组, 将当前位置之前的数加上后面元素的全排列加入到结果集中.
+     */
+    @Complexity(value = Complexity.ComplexityType.O_DEFINE, complexity = "O(n*n!)")
     public List<List<Integer>> permute(int[] nums) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        backTrack(result, nums, 0);
+        return result;
+    }
+
+    private void backTrack(List<List<Integer>> result, int[] nums, int current) {
+        if (current == nums.length) {
+            result.add(IntStream.of(nums).boxed().collect(Collectors.toList()));
+        }
+        for (int i = current; i < nums.length; i++) {
+            exchange(nums, current, i);
+            backTrack(result, nums, current + 1);
+            exchange(nums, current, i);
+        }
+    }
+
+    private void exchange(int[] nums, int a, int b) {
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
     }
 
 }
