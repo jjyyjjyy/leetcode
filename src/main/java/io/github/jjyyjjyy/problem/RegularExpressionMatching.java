bc89f912e5df2cfde75c56b708ff0bd785729b17
jy <977996067@qq.com>
2020-12-30 14:28:25 +0800

    :sparkles: Add regular-expression-matching solution

--- src/main/java/io/github/jjyyjjyy/problem/RegularExpressionMatching.java
+++ src/main/java/io/github/jjyyjjyy/problem/RegularExpressionMatching.java
@@ -1,5 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
@@ -79,7 +80,48 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class RegularExpressionMatching {
 
+    /**
+     * 1. 使用一个二维数组dp来保存s中m个字符与p中n个字符是否匹配:
+     * 1.1. 数组长度初始化为[m+1][n+1].
+     * 1.2. dp[0][0]初始化为true, 因为空串与空串可以匹配上.
+     * 1.3. dp第0列第行下的行均为false, dp第0行第0列右的每一列如果是普通字符为false, 如果为*号为上一个dp匹配结果(c*与空串是匹配的, 从而直接取dp[0][0]的匹配结果).
+     * 2. 依次从上往下, 从左往右遍历数组:
+     * 2.1. 如果s[i]==p[j], 那么dp[i][j]=dp[i-1][j-1].
+     * 2.2. 如果p[j]为*号(*号代表前一个字符可以重复0次或多次):
+     * 2.2.1. [重复0次的情况] 如果s[i]!=p[j-1], 因为*号表示p[j-1]是可以省略的, 所以需要匹配s[i]和[j-2], 之前循环已经匹配过了, 所以直接取前两步的匹配结果: dp[i][j]=dp[i][j-2].
+     * 2.2.2. [重复1次的情况] 如果s[i]==p[j-1], 那么直接取前一次的匹配结果: dp[i][j]=dp[i][j-2].
+     * 2.2.3. [重复多次的情况] 如果s[i]==p[j-1], 那么直接取s上一个字符的匹配结果: dp[i][j]=dp[i-j][j].
+     * 3. 最后dp[m][n]即为匹配结果.
+     */
+    @Complexity(Complexity.ComplexityType.O_M_N)
     public boolean isMatch(String s, String p) {
-        return false;
+        if (s == null || p == null) {
+            return false;
+        }
+        int sLength = s.length();
+        int pLength = p.length();
+
+        boolean[][] dp = new boolean[sLength + 1][pLength + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= pLength; i++) {
+            dp[0][i] = p.charAt(i - 1) == '*' && dp[0][i - 2];
+        }
+
+        for (int i = 1; i <= sLength; i++) {
+            for (int j = 1; j <= pLength; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    if (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') {
+                        dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
+                    } else {
+                        dp[i][j] = dp[i][j - 2];
+                    }
+                }
+                // 此处省略 else dp[i][j]=false, 因为boolean数组元素默认初始化值就是false.
+            }
+        }
+
+        return dp[sLength][pLength];
     }
 }
