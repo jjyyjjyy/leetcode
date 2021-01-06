9342d6fc8829e8641a9e627ddfc49590bf5efef7
jy <977996067@qq.com>
2021-01-06 15:41:56 +0800

    :sparkles: Add evaluate-division solution

--- src/main/java/io/github/jjyyjjyy/problem/EvaluateDivision.java
+++ src/main/java/io/github/jjyyjjyy/problem/EvaluateDivision.java
@@ -1,10 +1,11 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * <a href="https://leetcode-cn.com/problems/evaluate-division/">除法求值</a>
@@ -36,8 +37,94 @@ import java.util.List;
 )
 public class EvaluateDivision {
 
+    /**
+     * 广度优先遍历
+     * <p>
+     * 1. 记录所有出现的字符和对应的索引.
+     * 2. 创建一个图数组, 数组下标对应字符的索引, 每个顶点根据equations和values存放除数和商.
+     * 3. 遍历每个query中的字符a和字符b:
+     * 3.1. 如果字符a或b不存在, 当前计算结果为-1.0.
+     * 3.2. 如果a和b相等, 当前计算结果为1.0.
+     * 3.3. 以a的索引为起点, 根据图中a顶点BFS找到所有可达的顶点, 用乘积计算该顶点的值.
+     */
+    @Complexity(value = Complexity.ComplexityType.O_DEFINE, complexity = "O(ML+Q*(L+M))")
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        return null;
+
+        IntHolder intHolder = new IntHolder();
+
+        Map<String, Integer> variables = new HashMap<>();
+        equations.forEach(v -> {
+            variables.putIfAbsent(v.get(0), intHolder.value++);
+            variables.putIfAbsent(v.get(1), intHolder.value++);
+        });
+
+        int n = intHolder.value;
+
+        List<Pair>[] edges = new List[n];
+        for (int i = 0; i < n; i++) {
+            edges[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < equations.size(); i++) {
+            int va = variables.get(equations.get(i).get(0));
+            int vb = variables.get(equations.get(i).get(1));
+            edges[va].add(new Pair(vb, values[i]));
+            edges[vb].add(new Pair(va, 1 / values[i]));
+        }
+
+        int queryCount = queries.size();
+        double[] result = new double[queryCount];
+
+        for (int i = 0; i < queryCount; i++) {
+            List<String> currentQuery = queries.get(i);
+            String a = currentQuery.get(0);
+            String b = currentQuery.get(1);
+            if (!variables.containsKey(a) || !variables.containsKey(b)) {
+                result[i] = -1.0;
+                continue;
+            }
+            if (Objects.equals(a, b)) {
+                result[i] = 1.0;
+                continue;
+            }
+            int aIndex = variables.get(a);
+            int bIndex = variables.get(b);
+
+            Queue<Integer> points = new LinkedList<>();
+            points.add(aIndex);
+            double[] ratios = new double[n];
+            Arrays.fill(ratios, -1);
+            ratios[aIndex] = 1.0;
+
+            while (!points.isEmpty() && ratios[bIndex] < 0) {
+                Integer current = points.poll();
+                for (Pair edge : edges[current]) {
+                    int index = edge.index;
+                    if (ratios[index] < 0) {
+                        ratios[index] = ratios[current] * edge.value;
+                        points.add(index);
+                    }
+                }
+            }
+
+            result[i] = ratios[bIndex];
+        }
+
+        return result;
+    }
+
+    private static class Pair {
+
+        private int index;
+        private double value;
+
+        public Pair(int index, double value) {
+            this.index = index;
+            this.value = value;
+        }
+    }
+
+    private static class IntHolder {
+        private int value;
     }
 
 }
