80afe69d920a4c4499ddc7c8b8a87b3c46ec8a48
jy <977996067@qq.com>
2021-01-10 18:20:51 +0800

    :sparkles: Add best-time-to-buy-and-sell-stock-iii solution

--- src/main/java/io/github/jjyyjjyy/problem/BestTimeToBuyAndSellStockIII.java
+++ src/main/java/io/github/jjyyjjyy/problem/BestTimeToBuyAndSellStockIII.java
@@ -1,5 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
@@ -52,7 +53,27 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class BestTimeToBuyAndSellStockIII {
 
+    /**
+     * buy1 = max(buy1, -prices[i])
+     * sell1 = max(sell1, buy1 + prices[i])
+     * buy2 = max(buy2, sell1 - prices[i])
+     * sell2 = max(sell2, buy2 + prices[i])
+     */
+    @Complexity(Complexity.ComplexityType.O_N)
     public int maxProfit(int[] prices) {
-        return 0;
+        if (prices.length == 0) {
+            return 0;
+        }
+        int buy1 = -prices[0];
+        int sell1 = 0;
+        int buy2 = -prices[0];
+        int sell2 = 0;
+        for (int i = 1; i < prices.length; i++) {
+            buy1 = Math.max(buy1, -prices[i]);
+            sell1 = Math.max(sell1, buy1 + prices[i]);
+            buy2 = Math.max(buy2, sell1 - prices[i]);
+            sell2 = Math.max(sell2, buy2 + prices[i]);
+        }
+        return sell2;
     }
 }
