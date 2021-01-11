3aaa7d50f38e1d67aed81d2ca16f43c6c5b97d2e
jy <977996067@qq.com>
2021-01-11 11:26:46 +0800

    :sparkles: Add smallest-string-with-swaps solution

--- src/main/java/io/github/jjyyjjyy/problem/SmallestStringWithSwaps.java
+++ src/main/java/io/github/jjyyjjyy/problem/SmallestStringWithSwaps.java
@@ -1,10 +1,14 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 /**
  * <a href="https://leetcode-cn.com/problems/smallest-string-with-swaps/">交换字符串中的元素</a>
@@ -64,8 +68,82 @@ import java.util.List;
 )
 public class SmallestStringWithSwaps {
 
+    /**
+     * 算法: pairs即为连通分量, 使用并查集能得到连通的索引组, 然后将每组的字符使用最小堆排序, 依次取出就得到了最终排好序的字符串.
+     * <p>
+     * 1. 将pairs加入到并查集里.
+     * 2. 将每个元素的索引从并查集里拿到id, 插入到哈希表中, key为id, value为一个最小堆.
+     * 3. 依次遍历索引, 从哈希表中取出字符, 得到最小字典序的字符串.
+     */
+    @Complexity(value = Complexity.ComplexityType.O_DEFINE, complexity = "O((m+n)α(n)+nlogn)")
     public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
-        return null;
+        if (pairs.size() == 0) {
+            return s;
+        }
+
+        int n = s.length();
+        UnionFind unionFind = new UnionFind(n);
+        for (List<Integer> pair : pairs) {
+            unionFind.union(pair.get(0), pair.get(1));
+        }
+
+        char[] chars = s.toCharArray();
+        Map<Integer, PriorityQueue<Character>> map = new HashMap<>(n);
+        for (int i = 0; i < n; i++) {
+            int root = unionFind.find(i);
+            if (map.containsKey(root)) {
+                map.get(root).add(chars[i]);
+            } else {
+                map.computeIfAbsent(root, k -> new PriorityQueue<>()).add(chars[i]);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int root = unionFind.find(i);
+            result.append(map.get(root).poll());
+        }
+        return result.toString();
     }
 
+    /**
+     * Weighted quick-union
+     */
+    private static class UnionFind {
+
+        private final int[] id;
+        private final int[] rank;
+
+        public UnionFind(int n) {
+            this.id = new int[n];
+            this.rank = new int[n];
+            for (int i = 0; i < n; i++) {
+                this.id[i] = i;
+                this.rank[i] = 1;
+            }
+        }
+
+        public int find(int i) {
+            while (i != id[i]) {
+                i = id[i];
+            }
+            return i;
+        }
+
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX == rootY) {
+                return;
+            }
+            if (rank[rootX] == rank[rootY]) {
+                id[rootX] = rootY;
+                rank[rootY]++;
+            } else if (rank[rootX] < rank[rootY]) {
+                id[rootX] = rootY;
+            } else {
+                id[rootY] = rootX;
+            }
+        }
+    }
 }
