ada1375e2f21f3240c27bce132d104ec5b9b077b
jy <977996067@qq.com>
2021-01-20 10:12:18 +0800

    :sparkles: Add maximum-product-of-three-numbers solution

--- src/main/java/io/github/jjyyjjyy/problem/MaximumProductOfThreeNumbers.java
+++ src/main/java/io/github/jjyyjjyy/problem/MaximumProductOfThreeNumbers.java
@@ -1,8 +1,11 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
+
+import java.util.Arrays;
 
 /**
  * <a href="https://leetcode-cn.com/problems/maximum-product-of-three-numbers/">三个数的最大乘积</a>
@@ -44,8 +47,42 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class MaximumProductOfThreeNumbers {
 
+    /**
+     * 依次遍历数组, 找到最小的两个数和最大的三个数, 求乘积.
+     */
+    @Complexity(Complexity.ComplexityType.O_N)
     public int maximumProduct(int[] nums) {
-        return -1;
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (num > max3) {
+                max3 = num;
+            }
+        }
+        return Math.max(max1 * min1 * min2, max1 * max2 * max3);
     }
 
+    /**
+     * 1. 将数组排好序.
+     * 2. 3个数的最大乘积可能是最后三个数, 也可能是最后一个数乘以最小的两个负数.
+     */
+    @Complexity(Complexity.ComplexityType.O_N_LOG_N)
+    public int maximumProduct2(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return Math.max(nums[0] * nums[1] * nums[n - 1], nums[n - 1] * nums[n - 2] * nums[n - 3]);
+    }
 }
