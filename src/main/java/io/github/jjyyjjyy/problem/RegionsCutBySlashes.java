d311d32267470a53fc936fc68cb27d2f2a3b8050
jy <977996067@qq.com>
2021-01-25 10:36:34 +0800

    :sparkles: Add regions-cut-by-slashes solution

--- src/main/java/io/github/jjyyjjyy/problem/RegionsCutBySlashes.java
+++ src/main/java/io/github/jjyyjjyy/problem/RegionsCutBySlashes.java
@@ -1,5 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
@@ -93,7 +94,86 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class RegionsCutBySlashes {
 
+    /**
+     * 1. 将一个正方形按照上,右,下,左的顺序编好号, 一共4*n^2个号.
+     * 2. 创建一个并查集, 维护连通分量个数.
+     * 3. 遍历编号:
+     * 3.1. 2号和下方区域的0号相连.
+     * 3.2. 1号和右方区域的3号相连.
+     * 3.3. 如果是/, 则将0号和3号相连.
+     * 3.4. 如果是\, 则将1号和2号相连.
+     * 3.5. 如果是空格, 则将0,1,2号相连.
+     * 4. 最后并查集的连通分量的个数是区域的数目.
+     */
+    @Complexity(Complexity.ComplexityType.O_N_POW_2_LOG_N)
     public int regionsBySlashes(String[] grid) {
-        return -1;
+        int n = grid.length;
+        int pointCount = n * n * 4;
+        UnionFind unionFind = new UnionFind(pointCount);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int idx = i * n + j;
+                if (i < n - 1) {
+                    int bottom = idx + n;
+                    unionFind.union(idx * 4 + 2, bottom * 4);
+                }
+                if (j < n - 1) {
+                    int right = idx + 1;
+                    unionFind.union(idx * 4 + 1, right * 4 + 3);
+                }
+                if (grid[i].charAt(j) == '/') {
+                    unionFind.union(idx * 4, idx * 4 + 3);
+                    unionFind.union(idx * 4 + 1, idx * 4 + 2);
+                } else if (grid[i].charAt(j) == '\\') {
+                    unionFind.union(idx * 4, idx * 4 + 1);
+                    unionFind.union(idx * 4 + 2, idx * 4 + 3);
+                } else {
+                    unionFind.union(idx * 4, idx * 4 + 1);
+                    unionFind.union(idx * 4 + 1, idx * 4 + 2);
+                    unionFind.union(idx * 4 + 2, idx * 4 + 3);
+                }
+            }
+        }
+        return unionFind.setCount;
+    }
+
+    private static class UnionFind {
+        private final int[] ids;
+        private final int[] ranks;
+        private int setCount;
+
+        public UnionFind(int n) {
+            this.setCount = n;
+            this.ids = new int[n];
+            this.ranks = new int[n];
+            for (int i = 0; i < n; i++) {
+                this.ids[i] = i;
+                this.ranks[i] = 1;
+            }
+        }
+
+        public int find(int x) {
+            if (x != ids[x]) {
+                ids[x] = find(ids[x]);
+            }
+            return ids[x];
+        }
+
+        public void union(int x, int y) {
+            int xId = find(x);
+            int yId = find(y);
+            if (xId == yId) {
+                return;
+            }
+            if (ranks[xId] < ranks[yId]) {
+                int tmp = xId;
+                xId = yId;
+                yId = tmp;
+            }
+            ids[yId] = ids[xId];
+            ranks[xId] += ranks[yId];
+            setCount--;
+        }
     }
 }
