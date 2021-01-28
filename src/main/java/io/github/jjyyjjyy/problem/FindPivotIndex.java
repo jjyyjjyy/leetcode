0af807433592387b32b6eb059e67b1072a75c4ec
jy <977996067@qq.com>
2021-01-28 09:47:55 +0800

    :sparkles: Add find-pivot-index solution

--- src/main/java/io/github/jjyyjjyy/problem/FindPivotIndex.java
+++ src/main/java/io/github/jjyyjjyy/problem/FindPivotIndex.java
@@ -1,5 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
@@ -54,7 +55,24 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class FindPivotIndex {
 
+    /**
+     * 1. 先计算数组所有元素之和.
+     * 2. 依次遍历数组, 将总和减去当前元素, 然后判断是否与左边元素之和相等.
+     */
+    @Complexity(Complexity.ComplexityType.O_N)
     public int pivotIndex(int[] nums) {
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int num : nums) {
+            rightSum += num;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            rightSum -= nums[i];
+            if (leftSum == rightSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
         return -1;
     }
 }
