33a5e54e333ffa39dde855a9048794ff3cbd8f9e
jy <977996067@qq.com>
2021-02-10 21:20:57 +0800

    :sparkles: Add permutation-in-string solution

--- src/main/java/io/github/jjyyjjyy/problem/PermutationInString.java
+++ src/main/java/io/github/jjyyjjyy/problem/PermutationInString.java
@@ -1,5 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
@@ -52,7 +53,55 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class PermutationInString {
 
+    /**
+     * 1. 先统计出s1词频和字符种数.
+     * 2. 维护快慢两个指针, 依次遍历s2.
+     * 2.1. 如果当前字符在s1中, 则将s2窗口内当前字符的词频加1, 如果加到和s1中改字符的词频数相同, 则当前窗口中的字符种数加1.
+     * 2.2. 右指针向右移一位.
+     * 2.3. 如果窗口内词频等于s1的词频, 且窗口长度等于s1的长度, 则说明窗口内字符与s1是等价的, 返回true, 否则左指针右移一位, 并且窗口内的字符种数是否需要减1.
+     */
+    @Complexity(Complexity.ComplexityType.O_M_AND_N)
     public boolean checkInclusion(String s1, String s2) {
+
+        char[] s1Chars = s1.toCharArray();
+        char[] s2Chars = s2.toCharArray();
+
+        int wordCount = 0;
+        int[] wordFreq = new int[26];
+
+        for (char s1Char : s1Chars) {
+            if (wordFreq[s1Char - 'a']++ == 0) {
+                wordCount++;
+            }
+        }
+
+        int left = 0;
+        int right = 0;
+        int[] windowWordFreq = new int[26];
+        int windowWordCount = 0;
+        while (right < s2Chars.length) {
+            int index = s2Chars[right] - 'a';
+            if (wordFreq[index] > 0) {
+                windowWordFreq[index]++;
+                if (wordFreq[index] == windowWordFreq[index]) {
+                    windowWordCount++;
+                }
+            }
+            right++;
+            while (windowWordCount == wordCount) {
+                if (right - left == s1Chars.length) {
+                    return true;
+                }
+                int leftIndex = s2Chars[left] - 'a';
+                if (wordFreq[leftIndex] > 0) {
+                    windowWordFreq[leftIndex]--;
+                    if (windowWordFreq[leftIndex] < wordFreq[leftIndex]) {
+                        windowWordCount--;
+                    }
+                }
+                left++;
+            }
+        }
         return false;
     }
 }
