1570cdd28c7c7c4a93df9a17a226df650c1e9102
jy <977996067@qq.com>
2021-02-27 23:07:28 +0800

    :sparkles: Add longest-substring-with-at-least-k-repeating-characters solution

--- src/main/java/io/github/jjyyjjyy/problem/LongestSubstringWithAtLeastKRepeatingCharacters.java
+++ src/main/java/io/github/jjyyjjyy/problem/LongestSubstringWithAtLeastKRepeatingCharacters.java
@@ -1,5 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
@@ -41,8 +42,46 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class LongestSubstringWithAtLeastKRepeatingCharacters {
 
+    @Complexity(Complexity.ComplexityType.O_N)
     public int longestSubstring(String s, int k) {
-        return -1;
+        int n = s.length();
+        return dfs(s, 0, n - 1, k);
     }
 
+    private int dfs(String s, int l, int r, int k) {
+        int[] cnt = new int[26];
+        for (int i = l; i <= r; i++) {
+            cnt[s.charAt(i) - 'a']++;
+        }
+
+        char split = 0;
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] > 0 && cnt[i] < k) {
+                split = (char) (i + 'a');
+                break;
+            }
+        }
+        if (split == 0) {
+            return r - l + 1;
+        }
+
+        int i = l;
+        int ret = 0;
+        while (i <= r) {
+            while (i <= r && s.charAt(i) == split) {
+                i++;
+            }
+            if (i > r) {
+                break;
+            }
+            int start = i;
+            while (i <= r && s.charAt(i) != split) {
+                i++;
+            }
+
+            int length = dfs(s, start, i - 1, k);
+            ret = Math.max(ret, length);
+        }
+        return ret;
+    }
 }
