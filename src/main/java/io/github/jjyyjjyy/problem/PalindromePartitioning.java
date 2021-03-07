743631ce1fd1be991e4e184f487e5c85be3dd690
jy <977996067@qq.com>
2021-03-07 22:09:46 +0800

    :sparkles: Add palindrome-partitioning solution

--- src/main/java/io/github/jjyyjjyy/problem/PalindromePartitioning.java
+++ src/main/java/io/github/jjyyjjyy/problem/PalindromePartitioning.java
@@ -5,6 +5,8 @@ import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -39,7 +41,36 @@ public class PalindromePartitioning {
 
     @Complexity(value = Complexity.ComplexityType.O_DEFINE, complexity = "n*2^n")
     public List<List<String>> partition(String s) {
-        return null;
+        int n = s.length();
+        boolean[][] f = new boolean[n][n];
+        for (int i = 0; i < n; ++i) {
+            Arrays.fill(f[i], true);
+        }
+
+        List<List<String>> result = new ArrayList<>();
+        List<String> ans = new ArrayList<>();
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                f[i][j] = (s.charAt(i) == s.charAt(j)) && f[i + 1][j - 1];
+            }
+        }
+
+        dfs(result, ans, f, s, 0);
+        return result;
+    }
+
+    public void dfs(List<List<String>> result, List<String> ans, boolean[][] f, String s, int i) {
+        if (i == s.length()) {
+            result.add(new ArrayList<>(ans));
+            return;
+        }
+        for (int j = i; j < s.length(); ++j) {
+            if (f[i][j]) {
+                ans.add(s.substring(i, j + 1));
+                dfs(result, ans, f, s, j + 1);
+                ans.remove(ans.size() - 1);
+            }
+        }
     }
 
 }
