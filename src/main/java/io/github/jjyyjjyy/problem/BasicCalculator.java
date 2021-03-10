5f27fe419962b98ca5704b6a2263bbef6050f87d
jy <977996067@qq.com>
2021-03-10 11:37:41 +0800

    :sparkles: Add basic-calculator solution

--- src/main/java/io/github/jjyyjjyy/problem/BasicCalculator.java
+++ src/main/java/io/github/jjyyjjyy/problem/BasicCalculator.java
@@ -1,8 +1,11 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
+
+import java.util.Stack;
 
 /**
  * <a href="https://leetcode-cn.com/problems/basic-calculator/">基本计算器</a>
@@ -52,7 +55,63 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class BasicCalculator {
 
+    private static void cal(Stack<Integer> operands, Stack<Character> operators) {
+        if (operators.isEmpty()) {
+            return;
+        }
+        int b = operands.pop(), a = operands.pop();
+        operands.push(operators.pop() == '+' ? a + b : a - b);
+    }
+
+    /**
+     * 1. 维护一个操作数栈和操作符栈.
+     * 2. 遍历字符串:
+     * 2.1. 如果当前字符是空格, 则跳过.
+     * 2.2. 如果当前字符是数字, 则判断上一个字符是否是数字, 如果上一个字符是数字则将操作数栈栈顶弹出*10加上当前元素, 压入操作数栈中.
+     * 2.3. 如果当前字符是左括号, 直接压入操作符栈中.
+     * 2.4. 如果当前字符是右括号或者加减号, 则一直计算到左括号.
+     */
+    @Complexity(Complexity.ComplexityType.O_N)
     public int calculate(String s) {
-        return -1;
+        int n = s.length();
+        Stack<Integer> operands = new Stack<>();
+        Stack<Character> operators = new Stack<>();
+        if (s.charAt(0) == '-') {
+            operands.push(0);
+        }
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                continue;
+            }
+            if (c == '(') {
+                operators.push(c);
+            } else if (c == ')') {
+                while (!operators.isEmpty()) {
+                    if (operators.peek() != '(') {
+                        cal(operands, operators);
+                    } else {
+                        operators.pop();
+                        break;
+                    }
+                }
+            } else if (Character.isDigit(c)) {
+                int d = c - '0';
+                if (i > 0 && Character.isDigit(s.charAt(i - 1))) {
+                    d = operands.pop() * 10 + d;
+                }
+                operands.add(d);
+            } else {
+                // 一直算到左括号之前
+                while (!operators.isEmpty() && operators.peek() != '(') {
+                    cal(operands, operators);
+                }
+                operators.push(c);
+            }
+        }
+        while (!operators.isEmpty()) {
+            cal(operands, operators);
+        }
+        return operands.pop();
     }
 }
