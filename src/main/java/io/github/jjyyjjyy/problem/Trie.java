3159012322ad88770053a6c87837d718c5ca5aa8
jy <977996067@qq.com>
2021-04-14 23:20:59 +0800

    :sparkles: Add implement-trie-prefix-tree solution

--- src/main/java/io/github/jjyyjjyy/problem/Trie.java
+++ src/main/java/io/github/jjyyjjyy/problem/Trie.java
@@ -44,6 +44,10 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class Trie {
 
+    private final Trie[] children = new Trie[26];
+
+    private boolean end = false;
+
     /**
      * Initialize your data structure here.
      */
@@ -54,20 +58,44 @@ public class Trie {
      * Inserts a word into the trie.
      */
     public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.end = true;
     }
 
     /**
      * Returns if the word is in the trie.
      */
     public boolean search(String word) {
-        return false;
+        Trie trie = searchPrefix(word);
+        return trie != null && trie.end;
     }
 
     /**
      * Returns if there is any word in the trie that starts with the given prefix.
      */
     public boolean startsWith(String prefix) {
-        return false;
+        return searchPrefix(prefix) != null;
+    }
+
+    private Trie searchPrefix(String prefix) {
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return null;
+            }
+            node = node.children[index];
+        }
+        return node;
     }
 
 }
