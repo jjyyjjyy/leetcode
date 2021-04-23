c4b860af57ee7899a792bfe7555e41031569b43a
jy <977996067@qq.com>
2021-04-23 23:21:23 +0800

    :sparkles: Add largest-divisible-subset solution

--- src/main/java/io/github/jjyyjjyy/problem/LargestDivisibleSubset.java
+++ src/main/java/io/github/jjyyjjyy/problem/LargestDivisibleSubset.java
@@ -1,9 +1,13 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -35,7 +39,45 @@ import java.util.List;
 )
 public class LargestDivisibleSubset {
 
+    /**
+     * 1. 将数组排好序.
+     * 2. 维护一个dp数组, 存储当前元素可以整除右侧元素的数.
+     * 3. 依次遍历数组, 判断当前元素能不能整除后面的数, 如果可以, 加1. 同时维护dp数组里的最大值, 和该dp值对应的数组元素.
+     * 4. 从后向前遍历数组, 如果当前数可以被最大数整除, 则加入到结果集中, 更新最大的值为当前元素.
+     */
+    @Complexity(Complexity.ComplexityType.O_N_POW_2)
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        return null;
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int maxSize = 1;
+        int maxVal = dp[0];
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            if (dp[i] > maxSize) {
+                maxSize = dp[i];
+                maxVal = nums[i];
+            }
+        }
+        if (maxSize == 1) {
+            return Collections.singletonList(nums[0]);
+        }
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = n - 1; i >= 0 && maxSize > 0; i--) {
+            if (dp[i] == maxSize && maxVal % nums[i] == 0) {
+                result.add(nums[i]);
+                maxVal = nums[i];
+                maxSize--;
+            }
+        }
+        return result;
     }
 }
