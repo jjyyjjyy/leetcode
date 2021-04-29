932b5dcd2da7fdf06611916f71dbe636172809f6
jy <977996067@qq.com>
2021-04-29 23:22:44 +0800

    :sparkles: Add frog-jump solution

--- src/main/java/io/github/jjyyjjyy/problem/FrogJump.java
+++ src/main/java/io/github/jjyyjjyy/problem/FrogJump.java
@@ -1,5 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
@@ -54,8 +55,27 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class FrogJump {
 
+    /**
+     * dp[i][k] = dp[j][k-1] || dp[j][k] || dp[j][k+1]
+     */
+    @Complexity(Complexity.ComplexityType.O_N_POW_2)
     public boolean canCross(int[] stones) {
+        int n = stones.length;
+        boolean[][] dp = new boolean[n][n];
+        dp[0][0] = true;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                int k = stones[i] - stones[j];
+                if (k > j + 1) {
+                    break;
+                }
+                dp[i][k] = dp[j][k - 1] || dp[j][k] || dp[j][k + 1];
+                if (i == n - 1 && dp[i][k]) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
-
 }
