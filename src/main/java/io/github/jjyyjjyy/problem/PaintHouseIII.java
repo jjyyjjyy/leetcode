00618acf27a148719b569457c253cc5db2a40ac8
jy <977996067@qq.com>
2021-05-04 10:53:23 +0800

    :sparkles: Add paint-house-iii solution

--- src/main/java/io/github/jjyyjjyy/problem/PaintHouseIII.java
+++ src/main/java/io/github/jjyyjjyy/problem/PaintHouseIII.java
@@ -1,8 +1,11 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
+
+import java.util.Arrays;
 
 /**
  * <a href="https://leetcode-cn.com/problems/paint-house-iii/">粉刷房子 III</a>
@@ -75,7 +78,74 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class PaintHouseIII {
 
+    private static final int INFTY = Integer.MAX_VALUE / 2;
+
+    @Complexity(value = Complexity.ComplexityType.O_DEFINE, complexity = "O(m*n*target)")
     public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
-        return -1;
+        // 将颜色调整为从 0 开始编号，没有被涂色标记为 -1
+        for (int i = 0; i < m; ++i) {
+            --houses[i];
+        }
+
+        // dp 所有元素初始化为极大值
+        int[][][] dp = new int[m][n][target];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                Arrays.fill(dp[i][j], INFTY);
+            }
+        }
+        int[][][] best = new int[m][target][3];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < target; ++j) {
+                best[i][j][0] = best[i][j][2] = INFTY;
+                best[i][j][1] = -1;
+            }
+        }
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (houses[i] != -1 && houses[i] != j) {
+                    continue;
+                }
+
+                for (int k = 0; k < target; ++k) {
+                    if (i == 0) {
+                        if (k == 0) {
+                            dp[i][j][k] = 0;
+                        }
+                    } else {
+                        dp[i][j][k] = dp[i - 1][j][k];
+                        if (k > 0) {
+                            // 使用 best(i-1,k-1) 直接得到 dp(i,j,k) 的值
+                            int first = best[i - 1][k - 1][0];
+                            int firstIdx = best[i - 1][k - 1][1];
+                            int second = best[i - 1][k - 1][2];
+                            dp[i][j][k] = Math.min(dp[i][j][k], (j == firstIdx ? second : first));
+                        }
+                    }
+
+                    if (dp[i][j][k] != INFTY && houses[i] == -1) {
+                        dp[i][j][k] += cost[i][j];
+                    }
+
+                    // 用 dp(i,j,k) 更新 best(i,k)
+                    int first = best[i][k][0];
+                    int second = best[i][k][2];
+                    if (dp[i][j][k] < first) {
+                        best[i][k][2] = first;
+                        best[i][k][0] = dp[i][j][k];
+                        best[i][k][1] = j;
+                    } else if (dp[i][j][k] < second) {
+                        best[i][k][2] = dp[i][j][k];
+                    }
+                }
+            }
+        }
+
+        int ans = INFTY;
+        for (int j = 0; j < n; ++j) {
+            ans = Math.min(ans, dp[m - 1][j][target - 1]);
+        }
+        return ans == INFTY ? -1 : ans;
     }
 }
