0b7c459748989c374edd865cf54e3b3b4304a458
jy <977996067@qq.com>
2021-05-07 21:22:07 +0800

    :sparkles: xor-operation-in-an-array

--- src/main/java/io/github/jjyyjjyy/core/generate/ProblemGenerator.java
+++ src/main/java/io/github/jjyyjjyy/core/generate/ProblemGenerator.java
@@ -117,7 +117,7 @@ public class ProblemGenerator {
             .map(r -> ((JSONObject) r).getJSONObject("stat").getString("question__title_slug"))
             .forEach(ProblemGenerator::dumpToFile);*/
 
-        dumpToFile("decode-xored-array");
+        dumpToFile("xor-operation-in-an-array");
     }
 
     private static String capitalize(char cap) {
