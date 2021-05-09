86576268a70b88887492245f082e1b85851dff40
jy <977996067@qq.com>
2021-05-09 10:42:45 +0800

    :sparkles: find-minimum-time-to-finish-all-jobs

--- src/main/java/io/github/jjyyjjyy/core/generate/ProblemGenerator.java
+++ src/main/java/io/github/jjyyjjyy/core/generate/ProblemGenerator.java
@@ -117,7 +117,7 @@ public class ProblemGenerator {
             .map(r -> ((JSONObject) r).getJSONObject("stat").getString("question__title_slug"))
             .forEach(ProblemGenerator::dumpToFile);*/
 
-        dumpToFile("minimum-number-of-days-to-make-m-bouquets");
+        dumpToFile("find-minimum-time-to-finish-all-jobs");
     }
 
     private static String capitalize(char cap) {
