1aa8e5a85c68126c6bb0af223b43adc7be1341cf
jy <977996067@qq.com>
2021-05-23 22:02:17 +0800

    :sparkles: Add maximum-xor-with-an-element-from-array solution

--- src/main/java/io/github/jjyyjjyy/problem/MaximumXorWithAnElementFromArray.java
+++ src/main/java/io/github/jjyyjjyy/problem/MaximumXorWithAnElementFromArray.java
@@ -1,5 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
@@ -49,7 +50,53 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class MaximumXorWithAnElementFromArray {
 
+    @Complexity(value = Complexity.ComplexityType.O_DEFINE, complexity = "O((N+Q)⋅L)")
     public int[] maximizeXor(int[] nums, int[][] queries) {
-        return null;
+        Trie trie = new Trie();
+        for (int val : nums) {
+            trie.insert(val);
+        }
+        int numQ = queries.length;
+        int[] ans = new int[numQ];
+        for (int i = 0; i < numQ; ++i) {
+            ans[i] = trie.getMaxXorWithLimit(queries[i][0], queries[i][1]);
+        }
+        return ans;
+    }
+
+    private static class Trie {
+        private static final int L = 30;
+        private final Trie[] children = new Trie[2];
+        private int min = Integer.MAX_VALUE;
+
+        public void insert(int val) {
+            Trie node = this;
+            node.min = Math.min(node.min, val);
+            for (int i = L - 1; i >= 0; --i) {
+                int bit = (val >> i) & 1;
+                if (node.children[bit] == null) {
+                    node.children[bit] = new Trie();
+                }
+                node = node.children[bit];
+                node.min = Math.min(node.min, val);
+            }
+        }
+
+        public int getMaxXorWithLimit(int val, int limit) {
+            Trie node = this;
+            if (node.min > limit) {
+                return -1;
+            }
+            int ans = 0;
+            for (int i = L - 1; i >= 0; --i) {
+                int bit = (val >> i) & 1;
+                if (node.children[bit ^ 1] != null && node.children[bit ^ 1].min <= limit) {
+                    ans |= 1 << i;
+                    bit ^= 1;
+                }
+                node = node.children[bit];
+            }
+            return ans;
+        }
     }
 }
