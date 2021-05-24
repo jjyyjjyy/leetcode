79854823fcd69564b54ae8c036f0e1fb578fe844
jy <977996067@qq.com>
2021-05-24 21:18:26 +0800

    :sparkles: Add strange-printer solution

--- src/main/java/io/github/jjyyjjyy/problem/StrangePrinter.java
+++ src/main/java/io/github/jjyyjjyy/problem/StrangePrinter.java
@@ -1,5 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
@@ -47,7 +48,28 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class StrangePrinter {
 
+    /**
+     * 依次遍历数组, 如果s[i]==s[j], 那么dp[i][j]=dp[i][j-1]. 否则dp[i][j]=min(dp[i][k]+dp[k+1][j])
+     */
+    @Complexity(Complexity.ComplexityType.O_N_POW_3)
     public int strangePrinter(String s) {
-        return -1;
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i][j - 1];
+                } else {
+                    int min = Integer.MAX_VALUE;
+                    for (int k = i; k < j; k++) {
+                        min = Math.min(min, dp[i][k] + dp[k + 1][j]);
+                    }
+                    dp[i][j] = min;
+                }
+            }
+        }
+        return dp[0][n - 1];
     }
 }
