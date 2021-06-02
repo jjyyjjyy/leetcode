61067d1b9362af8f5948acf33244c0494c0e86c0
jy <977996067@qq.com>
2021-06-02 21:37:19 +0800

    :sparkles: Add continuous-subarray-sum solution

--- src/main/java/io/github/jjyyjjyy/problem/ContinuousSubarraySum.java
+++ src/main/java/io/github/jjyyjjyy/problem/ContinuousSubarraySum.java
@@ -1,8 +1,12 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <a href="https://leetcode-cn.com/problems/continuous-subarray-sum/">连续的子数组和</a>
@@ -44,7 +48,28 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class ContinuousSubarraySum {
 
+    /**
+     * 依次遍历数组, 判断是否之前出现过与当前相同的余数, 如果距离大于2则符合条件
+     */
+    @Complexity(Complexity.ComplexityType.O_N)
     public boolean checkSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        if (n < 2) {
+            return false;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int remainder = 0;
+        for (int i = 0; i < n; i++) {
+            remainder = (remainder + nums[i]) % k;
+            if (map.containsKey(remainder)) {
+                if (i - map.get(remainder) >= 2) {
+                    return true;
+                }
+            } else {
+                map.put(remainder, i);
+            }
+        }
         return false;
     }
 }
