1de932f0a26fedb1b5b1a0fb17625611aec92fa6
jy <977996067@qq.com>
2021-06-06 15:07:24 +0800

    :sparkles: Add ones-and-zeroes solution

--- src/main/java/io/github/jjyyjjyy/problem/OnesAndZeroes.java
+++ src/main/java/io/github/jjyyjjyy/problem/OnesAndZeroes.java
@@ -1,5 +1,6 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
@@ -52,8 +53,31 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class OnesAndZeroes {
 
+    @Complexity(value = Complexity.ComplexityType.O_DEFINE, complexity = "O(lmn+L)")
     public int findMaxForm(String[] strs, int m, int n) {
-        return -1;
+        int length = strs.length;
+        int[][][] dp = new int[length + 1][m + 1][n + 1];
+        for (int i = 1; i <= length; i++) {
+            int[] zerosOnes = getZerosOnes(strs[i - 1]);
+            int zeros = zerosOnes[0], ones = zerosOnes[1];
+            for (int j = 0; j <= m; j++) {
+                for (int k = 0; k <= n; k++) {
+                    dp[i][j][k] = dp[i - 1][j][k];
+                    if (j >= zeros && k >= ones) {
+                        dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j - zeros][k - ones] + 1);
+                    }
+                }
+            }
+        }
+        return dp[length][m][n];
     }
 
+    public int[] getZerosOnes(String str) {
+        int[] zerosOnes = new int[2];
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            zerosOnes[str.charAt(i) - '0']++;
+        }
+        return zerosOnes;
+    }
 }
