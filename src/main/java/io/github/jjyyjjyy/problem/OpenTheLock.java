5f2ee70ea6401def85af0c4afc05aeb3c4881023
jy <977996067@qq.com>
2021-06-25 22:24:39 +0800

    :sparkles: Add open-the-lock solution

--- src/main/java/io/github/jjyyjjyy/problem/OpenTheLock.java
+++ src/main/java/io/github/jjyyjjyy/problem/OpenTheLock.java
@@ -4,6 +4,8 @@ import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
 
+import java.util.*;
+
 /**
  * <a href="https://leetcode-cn.com/problems/open-the-lock/">打开转盘锁</a>
  *
@@ -73,7 +75,83 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class OpenTheLock {
 
+    /**
+     * A*启发式搜索
+     */
     public int openLock(String[] deadends, String target) {
+        if ("0000".equals(target)) {
+            return 0;
+        }
+        Set<String> dead = new HashSet<>();
+        for (String deadend : deadends) {
+            dead.add(deadend);
+        }
+        if (dead.contains("0000")) {
+            return -1;
+        }
+        PriorityQueue<AStar> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.f));
+        pq.offer(new AStar("0000", target, 0));
+        Set<String> seen = new HashSet<>();
+        seen.add("0000");
+
+        while (!pq.isEmpty()) {
+            AStar node = pq.poll();
+            for (String nextStatus : get(node.status)) {
+                if (!seen.contains(nextStatus) && !dead.contains(nextStatus)) {
+                    if (nextStatus.equals(target)) {
+                        return node.g + 1;
+                    }
+                    pq.offer(new AStar(nextStatus, target, node.g + 1));
+                    seen.add(nextStatus);
+                }
+            }
+        }
+
         return -1;
+    }
+
+    public char numPrev(char x) {
+        return x == '0' ? '9' : (char) (x - 1);
+    }
+
+    public char numSucc(char x) {
+        return x == '9' ? '0' : (char) (x + 1);
+    }
+
+    // 枚举 status 通过一次旋转得到的数字
+    public List<String> get(String status) {
+        List<String> ret = new ArrayList<>();
+        char[] array = status.toCharArray();
+        for (int i = 0; i < 4; ++i) {
+            char num = array[i];
+            array[i] = numPrev(num);
+            ret.add(new String(array));
+            array[i] = numSucc(num);
+            ret.add(new String(array));
+            array[i] = num;
+        }
+        return ret;
+    }
+}
+
+class AStar {
+    String status;
+    int f, g, h;
+
+    public AStar(String status, String target, int g) {
+        this.status = status;
+        this.g = g;
+        this.h = getH(status, target);
+        this.f = this.g + this.h;
+    }
+
+    // 计算启发函数
+    public static int getH(String status, String target) {
+        int ret = 0;
+        for (int i = 0; i < 4; ++i) {
+            int dist = Math.abs(status.charAt(i) - target.charAt(i));
+            ret += Math.min(dist, 10 - dist);
+        }
+        return ret;
     }
 }
