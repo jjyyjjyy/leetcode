e1c21f0913770c55374b9e2707d536c997ee2241
jy <977996067@qq.com>
2021-07-14 22:47:38 +0800

    :sparkles: Add minimum-absolute-difference solution

--- src/main/java/io/github/jjyyjjyy/problem/MinimumAbsoluteDifference.java
+++ src/main/java/io/github/jjyyjjyy/problem/MinimumAbsoluteDifference.java
@@ -1,8 +1,11 @@
 package io.github.jjyyjjyy.problem;
 
+import io.github.jjyyjjyy.core.Complexity;
 import io.github.jjyyjjyy.core.Difficulty;
 import io.github.jjyyjjyy.core.Problem;
 import io.github.jjyyjjyy.core.Tag;
+
+import java.util.Arrays;
 
 /**
  * <a href="https://leetcode-cn.com/problems/minimum-absolute-difference/">最小绝对差</a>
@@ -48,8 +51,42 @@ import io.github.jjyyjjyy.core.Tag;
 )
 public class MinimumAbsoluteDifference {
 
+    private static final int MOD = 1000000007;
+
+    @Complexity(Complexity.ComplexityType.O_N_LOG_N)
     public int minAbsoluteSumDiff(int[] nums1, int[] nums2) {
-        return -1;
+        int n = nums1.length;
+        int[] rec = new int[n];
+        System.arraycopy(nums1, 0, rec, 0, n);
+        Arrays.sort(rec);
+        int sum = 0, maxn = 0;
+        for (int i = 0; i < n; i++) {
+            int diff = Math.abs(nums1[i] - nums2[i]);
+            sum = (sum + diff) % MOD;
+            int j = binarySearch(rec, nums2[i]);
+            if (j < n) {
+                maxn = Math.max(maxn, diff - (rec[j] - nums2[i]));
+            }
+            if (j > 0) {
+                maxn = Math.max(maxn, diff - (nums2[i] - rec[j - 1]));
+            }
+        }
+        return (sum - maxn + MOD) % MOD;
     }
 
+    public int binarySearch(int[] rec, int target) {
+        int low = 0, high = rec.length - 1;
+        if (rec[high] < target) {
+            return high + 1;
+        }
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (rec[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
 }
